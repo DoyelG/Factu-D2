158ec3aa6856af18dcc1abc357a95a6c04d28e77
DoyelG <doyel@gm2dev.com>
2024-10-28 18:32:13 -0300

    Optimization

--- factuD2.py
+++ factuD2.py
@@ -1,21 +1,17 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support.ui import Select
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import Select, WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
 from pandas import *
-import time
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
 chrome_options.add_argument('--disable-gpu')
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-# driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-# options = webdriver.ChromeOptions()
-# options.add_argument("start-maximized")
-# options.add_argument("disable-infobars")
-# options.add_argument("--disable-extensions")
-
 
 accountData = read_csv("account.csv", dtype=str)
 CUIL = accountData["CUIL"].tolist()[0]
@@ -24,13 +20,11 @@ PASS = accountData["PASSWORD"].tolist()[0]
 companyData = read_csv("company.csv", dtype=str)
 COMPANY = companyData["COMPANY"].tolist()[0]
 
-
 class customer:
     def __init__(self, cuil, condition, amount):
         self.cuil = cuil
         self.condition = condition
         self.amount = amount
-
 
 customersData = read_csv("clients.csv", dtype=str)
 customersCuil = customersData["CUIL"].tolist()
@@ -46,28 +40,26 @@ for index, client in enumerate(customersCuil):
         customerCuil = ""
     clientsList.append(customer(customerCuil, customerCondition, customerAmount))
 
+def findElement(path, timeout = 10):
+    return WebDriverWait(driver, timeout).until(
+        EC.presence_of_element_located((By.XPATH, path))
+    )
 
-def findElement(path):
-    return driver.find_element("xpath", path)
+def findElements(path, timeout = 10):
+    return WebDriverWait(driver, timeout).until(
+        EC.presence_of_all_elements_located((By.XPATH, path))
+    )
 
-
-def findElements(path):
-    return driver.find_elements("xpath", path)
-
-
-def findElementAndClick(path):
-    element = driver.find_element("xpath", path)
+def findElementAndClick(path, timeout = 10):
+    element = WebDriverWait(driver, timeout).until(
+        EC.element_to_be_clickable((By.XPATH, path))
+    )
     element.click()
-    driver.implicitly_wait(10)
     return
-
 
 def nextStep():
-    nextStep = findElement("//input[@value='Continuar >']")
-    nextStep.click()
-    time.sleep(0.5)
+    findElementAndClick("//input[@value='Continuar >']")
     return
-
 
 def logIn():
     accountUsername = findElement("//input[@id='F1:username']")
@@ -78,15 +70,16 @@ def logIn():
     findElementAndClick("//input[@id='F1:btnIngresar']")
     findElementAndClick("//input[@value='" + COMPANY + "']")
 
-
 def generateInvoice(cuil, condition, amount):
     findElementAndClick("//a[@id='btn_gen_cmp']")
-    time.sleep(0.9)
     sellPoint = Select(findElement("//select[@id='puntodeventa']"))
-    time.sleep(0.9)
     sellPoint.select_by_index("1")
-    time.sleep(0.5)
-    nextStep()
+    try:
+        findElementAndClick("//input[@id='novolveramostrar']", 0.5)
+    except TimeoutException:
+        print("Attack 'dont show again' was very effective!")
+    finally:
+        nextStep()
 
     concept = Select(findElement("//select[@id='idconcepto']"))
     concept.select_by_index("2")
@@ -95,7 +88,6 @@ def generateInvoice(cuil, condition, amount):
         "//td[contains(@class, 'day') and not(contains(@class, 'othermonth')) and not(contains(@class, 'wn')) and not(contains(@class, 'name'))]"
     )
     currentMonthDaysSince[0].click()
-    time.sleep(0.5)
     findElementAndClick("//input[@id='fsh_btn']")
     currentMonthDaysTo = findElements(
         "//td[contains(@class, 'day') and not(contains(@class, 'othermonth')) and not(contains(@class, 'wn')) and not(contains(@class, 'name'))]"
@@ -104,28 +96,23 @@ def generateInvoice(cuil, condition, amount):
     nextStep()
 
     conditionIVA = Select(findElement("//select[@id='idivareceptor']"))
-    conditionIVA.select_by_index(condition)
+    conditionIVA.select_by_index(int(condition))
     cuilInput = findElement("//input[@id='nrodocreceptor']")
     cuilInput.send_keys(cuil)
     findElementAndClick("//form[@id='formulario']")
-    time.sleep(0.5)
     findElementAndClick("//input[@id='formadepago4']")
     nextStep()
 
     service = findElement("//textarea[@id='detalle_descripcion1']")
     service.send_keys("Servicios Informaticos")
-    driver.implicitly_wait(100)
     seleccionUnidad = Select(findElement("//select[@id='detalle_medida1']"))
     seleccionUnidad.select_by_index("7")
-    driver.implicitly_wait(100)
     amountInput = findElement("//input[@id='detalle_precio1']")
     amountInput.send_keys(amount)
     nextStep()
 
     findElementAndClick("//input[@id='btngenerar']")
-    time.sleep(0.9)
     driver.switch_to.alert.accept()
-    time.sleep(0.9)
 
     findElementAndClick("//input[@value='Menú Principal']")
 
